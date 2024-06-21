86e4d5d7517409537a52442173b74abbb37eb53d
zhuay <120068512+zhuay-a@users.noreply.github.com>
2024-06-21 14:45:39 +0800

    Update GoodsController.java

--- src/main/java/com/example/thread_safe_online/controller/GoodsController.java
+++ src/main/java/com/example/thread_safe_online/controller/GoodsController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 
 import java.sql.SQLException;
 
