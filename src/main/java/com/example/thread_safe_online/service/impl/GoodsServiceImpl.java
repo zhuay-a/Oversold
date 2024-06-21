5dcc00ea6a4c6ab7cb83be5a84529257f540b875
zhuay <120068512+zhuay-a@users.noreply.github.com>
2024-06-21 17:53:45 +0800

    Update GoodsServiceImpl.java

--- src/main/java/com/example/thread_safe_online/service/impl/GoodsServiceImpl.java
+++ src/main/java/com/example/thread_safe_online/service/impl/GoodsServiceImpl.java
@@ -280,17 +280,4 @@ public class GoodsServiceImpl implements GoodsService {
         }
     }
 
-    public void testTCA() {
-        int i = 1, j = 0;
-        i = i / j;
-        int x = 2, y = 3;
-        while(x ++ < y ++) {
-            if(x >= 100000)
-            {
-                x = 2, y = 3;
-            }
-        }
-        
-    }
-
 }
